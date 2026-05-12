b7d0ee20db99404bf21b1dc82600001590c8b8bd
RIOSNV <rr8972114@gmail.com>
2026-05-12 11:02:00 -0600

    Test: HelloWorldController.java

---
+++ src/main/java/mx/edu/tecdesoftware/market_backend/HelloWorldController.java
@@ -0,0 +1,10 @@
+package mx.edu.tecdesoftware.market_backend;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloWorldController{
+   @GetMapping("/saludar")
+    public String sayHello(){
+        return "Hello World";
+    }
+}
